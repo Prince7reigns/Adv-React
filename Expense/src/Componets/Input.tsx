e19a8dd3e08a8d7517416a5e8124e17492b11e89
Prince Yadav <prinsyadac@gmail.com>
2026-06-24 13:34:10 +0530

    feat: add reusable Input component with TypeScript support

---
+++ Expense/src/Componets/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+
+const Input = ({...props}:InputProps) => {
+  return (
+    <input {...props}/>
+  )
+}
+
+export default Input
