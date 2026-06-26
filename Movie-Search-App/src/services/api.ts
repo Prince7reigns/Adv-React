5581ac5b769178c5cab8ff09a55612451b695d93
Prince Yadav <prinsyadac@gmail.com>
2026-06-26 23:12:05 +0530

    feat: add initial API service file for OMDB API integration

---
+++ Movie-Search-App/src/services/api.ts
@@ -0,0 +1,16 @@
+// note
+
+// const BASE_URL = "https://www.omdbapi.com";
+// const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+// export async function request(params: string) {
+//   const response = await fetch(
+//     `${BASE_URL}/?apikey=${API_KEY}&${params}`
+//   );
+
+//   if (!response.ok) {
+//     throw new Error("Network Error");
+//   }
+
+//   return response.json();
+// }
