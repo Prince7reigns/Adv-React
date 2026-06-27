7118cd5dbe5309fffb628847bdeb91e7b78b7cdc
Prince Yadav <prinsyadac@gmail.com>
2026-06-28 03:24:58 +0530

    feat: implement wishlist functionality in App component with localStorage support

--- Movie-Search-App/src/App.tsx
+++ Movie-Search-App/src/App.tsx
@@ -6,8 +6,49 @@ import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
 import Favorites from "./pages/Favorites";
+import { useEffect, useState } from "react";
+import type { MovieSummary } from "./types/movie";
 
 function App() {
+
+  const [wishlist,setWishlist]=useState<MovieSummary[]>(()=>{
+    const wishli = localStorage.getItem('wishlist')
+
+    return wishli ?
+        JSON.parse(wishli) 
+        :
+       []
+  })
+
+  function handlewishlist(movie:MovieSummary){
+
+    const newMovie = {
+      Title:movie.Title,
+      Poster:movie.Poster,
+      imdbID:movie.imdbID,
+      Type:movie.Type,
+      Year:movie.Year
+    }
+
+    setWishlist((prev) => {
+    const exists  =   prev.some((m) => m.imdbID === movie.imdbID);
+
+    if(exists){
+      return prev.filter((m)=>m.imdbID !== movie.imdbID)
+    }
+
+    return [...prev, newMovie];
+  });
+  }
+
+  useEffect(()=>{
+
+     localStorage.setItem(
+        'wishlist',
+        JSON.stringify(wishlist)
+      );
+
+  },[wishlist])
 
   return (
   
@@ -16,9 +57,9 @@ function App() {
 
      <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="movies" element={<Movies/>} />
-      <Route path="movie/:id" element={<MovieDetails/>} />
-      <Route path="favorites" element={<Favorites/>} />
+      <Route path="movies" element={<Movies wishlist={wishlist} onToggleWishlist={handlewishlist}/>} />
+      <Route path="movie/:id" element={<MovieDetails wishlist={wishlist} onToggleWishlist={handlewishlist} />} />
+      <Route path="favorites" element={<Favorites wishlist={wishlist} onToggleWishlist={handlewishlist}/>} />
      </Routes>
   </BrowserRouter>
   )
